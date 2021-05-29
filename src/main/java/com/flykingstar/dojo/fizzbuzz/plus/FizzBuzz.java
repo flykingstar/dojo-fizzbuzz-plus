ebde259ce8ce0c95c04b7a543bd0695781a6689a
dengzhe <87505192@qq.com>
2021-05-29 17:37:02 +0800

    feat:完成

--- src/main/java/com/flykingstar/dojo/fizzbuzz/plus/FizzBuzz.java
+++ src/main/java/com/flykingstar/dojo/fizzbuzz/plus/FizzBuzz.java
@@ -6,4 +6,21 @@ package com.flykingstar.dojo.fizzbuzz.plus;
  */
 public class FizzBuzz {
 
+    public static String of(Integer number) {
+        String result = "";
+        if (isRelated(number, 3)) {
+            result = result + "fizz";
+        }
+        if (isRelated(number, 5)) {
+            result = result + "buzz";
+        }
+        if (result.length() == 0) {
+            result = String.valueOf(number);
+        }
+        return result;
+    }
+
+    private static boolean isRelated(Integer number, int operator) {
+        return number % operator == 0 || String.valueOf(number).contains(String.valueOf(operator));
+    }
 }
--- src/test/java/com/flykingstar/dojo/fizzbuzz/plus/FizzBuzzTests.java
+++ src/test/java/com/flykingstar/dojo/fizzbuzz/plus/FizzBuzzTests.java
@@ -1,5 +1,25 @@
 package com.flykingstar.dojo.fizzbuzz.plus;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class FizzBuzzTest {
 
+    @ParameterizedTest(name = "when given {0} should return {1}")
+    @CsvSource({
+            "1,'1'",
+            "2,'2'",
+            "3,'fizz'",
+            "5,'buzz'",
+            "6,'fizz'",
+            "15,'fizzbuzz'",
+            "13,'fizz'",
+            "300,'fizzbuzz'",
+            "51,'fizzbuzz'"
+    })
+    void should_return_correct_string_by_given(Integer number,String result){
+        assertThat(FizzBuzz.of(number)).isEqualTo(result);
+    }
 }
